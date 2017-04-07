4f7ec6d788bdd83d5e09c775e24dd5bc7f4d1e4e
kndllbrwn <kndllbrwn1914@gmail.com>
2017-04-07 18:51:19 -0400

    Created Utils file to hold reusable functions

--- js/bootstrap/Button.js
+++ js/bootstrap/Button.js
@@ -1,17 +1,14 @@
 import React from 'react'
+import { classify } from './Utils'
 
 export default class Button extends React.Component{
-  classify(key, value){
-    if (value[0] === '-') value = key+value;
-    value = value.split(' -').join(` ${key}-`)
-    return `${key} ${value}`;
-  }
+  
   render(){
     {/*var { className, href, target } = this.props
      can verbosely assign props with className={className} href={href} target={target} 
     const className = 'btn ' + this.props.className;*/}
     const Tag = this.props.href ? 'a':'button';
-    const className = this.classify('btn', this.props.className) + (this.props.disabled ? ' disabled':'');
+    const className = classify('btn', this.props.className) + (this.props.disabled ? ' disabled':'');
     
     return(
       <Tag {...this.props} className={className}>Discover things</Tag>
